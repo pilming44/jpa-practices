d5b60e3edb9e879c4161b1ff35360008d12c0542
pilming44 <pilming44@naver.com>
2024-04-22 17:26:56 +0900

    출판사 등록 구현

--- src/main/java/com/practice/jpa/controller/PublisherController.java
+++ src/main/java/com/practice/jpa/controller/PublisherController.java
@@ -1,15 +1,29 @@
 package com.practice.jpa.controller;
 
+import com.practice.jpa.controller.form.PublisherForm;
+import com.practice.jpa.entity.Publisher;
+import com.practice.jpa.service.PublisherService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class PublisherController {
+    private final PublisherService publisherService;
     @GetMapping("/publishers/new")
     public String registerpublisherForm() {
         log.info("출판사 등록 폼");
         return "publishers/registerPublisherForm";
+    }
+
+    @PostMapping ("/publishers/new")
+    public String registerpublisher(PublisherForm publisherForm) {
+        log.info("출판사 등록");
+        publisherService.save(new Publisher(publisherForm.getName()));
+        return "redirect:/";
     }
 }
