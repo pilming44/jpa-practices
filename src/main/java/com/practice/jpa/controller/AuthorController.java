b23919ed1a7ca19415ce8a95db5936a3de973227
pilming44 <pilming44@naver.com>
2024-04-22 17:23:02 +0900

    저자 등록 구현

--- src/main/java/com/practice/jpa/controller/AuthorController.java
+++ src/main/java/com/practice/jpa/controller/AuthorController.java
@@ -1,16 +1,29 @@
 package com.practice.jpa.controller;
 
+import com.practice.jpa.controller.form.AuthorForm;
+import com.practice.jpa.entity.Author;
+import com.practice.jpa.service.AuthorService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class AuthorController {
-
+    private final AuthorService authorService;
     @GetMapping("/authors/new")
-    public String registerAuthorFrom() {
+    public String registerAuthorForm() {
         log.info("저자등록 폼");
         return "authors/registerAuthorForm";
+    }
+
+    @PostMapping("/authors/new")
+    public String registerAuthor(AuthorForm authorForm) {
+        log.info("저자등록 post");
+        authorService.saveAuthor(new Author(authorForm.getName()));
+        return "redirect:/";
     }
 }
